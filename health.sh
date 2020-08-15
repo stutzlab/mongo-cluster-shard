85977e3b0f3111821a2cd42e9b8eddc1a8dfa91f
flaviostutz <flaviostutz@gmail.com>
2020-08-15 12:13:31 -0300

    refactoring healthcheck

--- health.sh
+++ health.sh
@@ -1,26 +1,36 @@
 #!/bin/sh
 
-set -e
+function deepCheck() {
+    #check for replica set status
+    R=$(echo 'rs.status().ok' | mongo localhost:27017/test --quiet)
+    echo $R | grep 1
+    if [ "$?" == "0" ]; then
+        return 0
+    fi
 
+    #check that this node appears in nodes list
+    mongo --eval "db.isMaster()" | grep $1
+    if [ "$?" == "0" ]; then
+        return 0
+    fi
+
+    return 1
+}
+
+set -e
 nc -z 127.0.0.1 27017
+set +e
 
 if [ ! -f "/data/__initialized" ]; then
     echo "Node not initialized yet"
-    set +e
-    R=$(echo 'rs.status().ok' | mongo localhost:27017/test --quiet)
-    E=$?
-    if [ "$E" = "0" ]; then
-        if [ "$R" = "1" ]; then
-            touch /data/__initialized
-        fi
+    R=$(deepCheck $SHARD_REPLICA_SET)
+    if [ "$R" = "0" ]; then
+        touch /data/__initialized
     fi
-
-else
-    echo "Node already initialized. make deep check"
-    #check for replica set status
-    R=$(echo 'rs.status().ok' | mongo localhost:27017/test --quiet)
-    echo $R | grep 1 
-
-    #check that this node appears in nodes list
-    mongo --eval "db.isMaster()" | grep $SHARD_REPLICA_SET
+    exit 0
 fi
+
+echo "Node already initialized. make deep check"
+R=$(deepCheck $SHARD_REPLICA_SET)
+exit $R
+
